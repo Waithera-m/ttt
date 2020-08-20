f2f068daab6e55b8fb439bf35000ac0ef6e0cb95
Mary Waithera <njihiamary11@gmail.com>
2020-08-20 15:12:08 +0300

    Create components to handle different aspects of the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="jumbotron shadow shadow-lg mb-5" style={{backgroundColor: "#4AD3C5", color:"white"}}>
+        <h2 className="display-4 text-center">Tic-Tac-Toe</h2> 
       </header>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-7">
+          <Game />
+        </div>
+        <div className="col-md-1"></div>
     </div>
+      
+    </div>
+    
+
+    
   );
+  
+
+  
+
+  
+
+  
 }
+
 
 export default App;
