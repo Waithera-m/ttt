a5be1ed7d092511f1526529208a69ce6a671c577
Mary Waithera <njihiamary11@gmail.com>
2020-08-25 12:34:32 +0300

    Add tests

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <header className="jumbotron shadow shadow-lg mb-5" style={{backgroundColor: "#4AD3C5", color:"white"}}>
-        <h2 className="display-4 text-center">Tic-Tac-Toe</h2> 
+        <h2 className="display-4 text-center">Welcome to Tic-Tac-Toe</h2> 
       </header>
       <div className="row">
         <div className="col-md-4"></div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Tic-Tac-Toe/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('component renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<App />, div);
 });
